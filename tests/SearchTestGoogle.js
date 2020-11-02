d97fab2f4ae645eabd46ff5c44f9b9c1112dc5f6
Cerafima4 <cerafima4@gmail.com>
2020-11-02 19:43:23 +0700

    Добавлен автотест ориентированный на поиск запросов в браузере Google

---
+++ tests/SearchTestGoogle.js
@@ -0,0 +1,21 @@
+module.exports = {
+	'перейти на сайт google.com': function (browser) {
+		browser
+			.url('https://www.google.com/')
+			.waitForElementVisible('#tsf > div:nth-child(2) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input', 5000);
+	},
+
+	'ввести запрос в поисковую строку google': function (browser) {
+		browser
+			.setValue('#tsf > div:nth-child(2) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input', 'огурец')
+			.click('#tsf > div:nth-child(2) > div.A8SBwf.emcav > div.UUbT9 > div.aajZCb > div.tfB0Bf > center > input.gNO89b')
+			.waitForElementVisible('#logo > img',5000);
+
+	},
+	'проверяем наличие количества результатов поиска': function (browser) {
+		browser
+			.assert
+			.elementPresent('#result-stats')
+			.end();
+	}
+}
