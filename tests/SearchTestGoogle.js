cdbcc8d53b9185e118b7e6d0a4f84d5a3660f88d
Nikolay Simonenko <n.simonenko@robo.finance>
2021-01-19 21:52:25 +0700

    изменили название шага

--- tests/SearchTestGoogle.js
+++ tests/SearchTestGoogle.js
@@ -2,7 +2,11 @@ module.exports = {
 	'перейти на сайт google.com': function (browser) {
 		browser
 			.url('https://www.google.com/')
-			.waitForElementVisible('#tsf > div:nth-child(2) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input', 5000);
+			.waitForElementVisible(
+				'cssSelector',
+				'#tsf > div:nth-child(2) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input',
+				5000
+			);
 	},
 
 	'ввести запрос в поисковую строку google': function (browser) {
@@ -12,7 +16,7 @@ module.exports = {
 			.waitForElementVisible('#logo > img',5000);
 
 	},
-	'проверяем наличие количества результатов поиска': function (browser) {
+	'проверяем наличие результатов поиска': function (browser) {
 		browser
 			.assert
 			.elementPresent('#result-stats')
